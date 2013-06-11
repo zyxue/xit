4b78be929868ea3c01b4a376dea70d39ada17c39
Zhuyi Xue (MP2) <zhuyi.xue@mail.utoronto.ca>
2013-06-11 15:26:05 -0400

    added decorate_ax for rama plot_type

--- plot_types/rama.py
+++ plot_types/rama.py
@@ -23,15 +23,16 @@ def rama(data, A, C, **kw):
         ax = fig.add_subplot(row, col, c+1)
         da = data[gk]
 
-        psis, phis = da # the order of phi, psi produced by g_rama is probably wrong. --2013-06-11
+        # the order of phi, psi produced by g_rama is probably
+        # wrong. --2013-06-11
+        psis, phis = da
         h, phip, psip = np.histogram2d(phis, psis, range=[[-180,180], [-180,180]], bins=36)
         phip = (phip[1:] + phip[:-1]) / 2.
         psip = (psip[1:] + psip[:-1]) / 2.
         contour = ax.contourf(phip, psip, h, cmap=cm.gray_r)
-
-        ax.set_xlim([-200, 200])
-        ax.set_ylim([-200, 200])
     
+        decorate_ax(ax, pt_dd, gk, A)
+
     # plt.colorbar(contour, shrink=0.6, extend='both')
     plt.savefig(U.gen_output_filename(A, C))
 
@@ -41,3 +42,21 @@ def rama(data, A, C, **kw):
     # else:
     #     # assume usually 36 bins would be enough
     #     bins = np.linspace(_la.min(), _la.max(), 36)
+
+def decorate_ax(ax, pt_dd, gk, A):
+    if 'xlabel' in pt_dd: ax.set_xlabel(pt_dd['xlabel'])
+    if 'ylabel' in pt_dd: ax.set_ylabel(pt_dd['ylabel'])
+    
+    if 'xlim' in pt_dd:
+        ax.set_xlim(**pt_dd['xlim'])
+    else:
+        ax.set_xlim([-200, 200])
+    if 'ylim' in pt_dd:
+        ax.set_ylim(**pt_dd['ylim'])
+    else:
+        ax.set_ylim([-200, 200])
+
+    if 'titles' in pt_dd:
+        ax.set_title(U.get_param(pt_dd['titles'], gk))
+    else:
+        ax.set_title(gk + '_' + A.property)
