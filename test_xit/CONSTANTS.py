9fae5f2a25e7d6c64c8f7bc77c7f70d284800df4
Zhuyi Xue (Desktop) <zhuyi.xue@mail.utoronto.ca>
2013-11-14 17:10:31 -0500

    learning setUp and tearDown

--- test_xit/CONSTANTS.py
+++ test_xit/CONSTANTS.py
@@ -1,44 +1,56 @@
 CORE_VARS = [
-    {'dir1': 'test_epz1', 'dir2': 'h', 'dir3': '00',
-     'path1': 'test_epz1', 'path2': 'test_epz1/h', 'path3': 'test_epz1/h/00',
-     'var1': 'test_epz1', 'var2': 'h', 'var3': 8, 'var4': '00',
-     'id_': 'test_epz1h8_00'
+    {'dir1': 'lele_epz1', 'dir2': 'h', 'dir3': '00',
+     'path1': 'lele_epz1', 'path2': 'lele_epz1/h', 'path3': 'lele_epz1/h/00',
+     'var1': 'lele_epz1', 'var2': 'h', 'var3': 8, 'var4': '00',
+     'id_': 'lele_epz1h8_00'
      },
-    {'dir1': 'test_epz1', 'dir2': 'e', 'dir3': '00',
-     'path1': 'test_epz1', 'path2': 'test_epz1/e', 'path3': 'test_epz1/e/00',
-     'var1': 'test_epz1',  'var2': 'e', 'var3': 8, 'var4': '00',
-     'id_': 'test_epz1e8_00'
+    {'dir1': 'lele_epz1', 'dir2': 'e', 'dir3': '00',
+     'path1': 'lele_epz1', 'path2': 'lele_epz1/e', 'path3': 'lele_epz1/e/00',
+     'var1': 'lele_epz1',  'var2': 'e', 'var3': 8, 'var4': '00',
+     'id_': 'lele_epz1e8_00'
      },
-    {'dir1': 'test_epz1', 'dir2': 'h', 'dir3': '01', 
-     'path1': 'test_epz1', 'path2': 'test_epz1/h', 'path3': 'test_epz1/h/01',
-     'var1': 'test_epz1', 'var2': 'h', 'var3': 8, 'var4': '01', 
-      'id_': 'test_epz1h8_01'
+    {'dir1': 'lele_epz1', 'dir2': 'h', 'dir3': '01', 
+     'path1': 'lele_epz1', 'path2': 'lele_epz1/h', 'path3': 'lele_epz1/h/01',
+     'var1': 'lele_epz1', 'var2': 'h', 'var3': 8, 'var4': '01', 
+      'id_': 'lele_epz1h8_01'
      },
-    {'dir1': 'test_epz1', 'dir2': 'e', 'dir3': '01',
-     'path1': 'test_epz1', 'path2': 'test_epz1/e', 'path3': 'test_epz1/e/01',
-     'var1': 'test_epz1', 'var2': 'e', 'var3': 8, 'var4': '01',
-     'id_': 'test_epz1e8_01'
+    {'dir1': 'lele_epz1', 'dir2': 'e', 'dir3': '01',
+     'path1': 'lele_epz1', 'path2': 'lele_epz1/e', 'path3': 'lele_epz1/e/01',
+     'var1': 'lele_epz1', 'var2': 'e', 'var3': 8, 'var4': '01',
+     'id_': 'lele_epz1e8_01'
      },
-    {'dir1': 'test_epz1', 'dir2': 'h', 'dir3': '02',
-     'path1': 'test_epz1', 'path2': 'test_epz1/h', 'path3': 'test_epz1/h/02',
-     'var1': 'test_epz1', 'var2': 'h', 'var3': 8, 'var4': '02',
-     'id_': 'test_epz1h8_02'
+    {'dir1': 'lele_epz1', 'dir2': 'h', 'dir3': '02',
+     'path1': 'lele_epz1', 'path2': 'lele_epz1/h', 'path3': 'lele_epz1/h/02',
+     'var1': 'lele_epz1', 'var2': 'h', 'var3': 8, 'var4': '02',
+     'id_': 'lele_epz1h8_02'
      },
-    {'dir1': 'test_epz1', 'dir2': 'e', 'dir3': '02', 
-     'path1': 'test_epz1', 'path2': 'test_epz1/e', 'path3': 'test_epz1/e/02',
-     'var1': 'test_epz1', 'var2': 'e', 'var3': 8, 'var4': '02',
-     'id_': 'test_epz1e8_02'}
+    {'dir1': 'lele_epz1', 'dir2': 'e', 'dir3': '02', 
+     'path1': 'lele_epz1', 'path2': 'lele_epz1/e', 'path3': 'lele_epz1/e/02',
+     'var1': 'lele_epz1', 'var2': 'e', 'var3': 8, 'var4': '02',
+     'id_': 'lele_epz1e8_02'}
     ]
 
 DIR_DICT = {
-    1: set(['test_epz1']),
-    2: set(['test_epz1/e', 'test_epz1/h']),
-    3: set(['test_epz1/e/00', 'test_epz1/e/01', 'test_epz1/e/02',
-            'test_epz1/h/00', 'test_epz1/h/01', 'test_epz1/h/02',
+    1: set(['lele_epz1']),
+    2: set(['lele_epz1/e', 'lele_epz1/h']),
+    3: set(['lele_epz1/e/00', 'lele_epz1/e/01', 'lele_epz1/e/02',
+            'lele_epz1/h/00', 'lele_epz1/h/01', 'lele_epz1/h/02',
             ])
     }
 
-DIR_LIST =  ['test_epz1', 
-             'test_epz1/e', 'test_epz1/h', 
-             'test_epz1/e/00', 'test_epz1/e/01', 'test_epz1/e/02',
-             'test_epz1/h/00', 'test_epz1/h/01', 'test_epz1/h/02']
+DIR_LIST =  ['lele_epz1', 
+             'lele_epz1/e', 'lele_epz1/h', 
+             'lele_epz1/e/00', 'lele_epz1/e/01', 'lele_epz1/e/02',
+             'lele_epz1/h/00', 'lele_epz1/h/01', 'lele_epz1/h/02']
+
+C = {
+    'prep': {
+        'sed_templates': {
+            'top': 'repository/top.template',
+            'pre_mdrun': 'repository/0_pre_mdrun.sh.template',
+            'mdrun': 'repository/0_mdrun.sh.template',
+            }
+        }
+    }
+
+        
--- test_xit/test_prep.py
+++ test_xit/test_prep.py
@@ -16,24 +16,31 @@ import CONSTANTS as const
 class PrepTestCase(unittest.TestCase):
     def setUp(self):
         self.core_vars = const.CORE_VARS
+        self.C = const.C
         
     def testMkdir(self):
         paths = const.DIR_LIST
         prep.mkdir(self.core_vars)
         for p in paths:
             self.assertTrue(os.path.exists(p))
-    
-    def tearDown(self):
-        paths = const.DIR_DICT
-        eq_dir_name = S.EQ_DIR_NAME
 
-        depths = sorted(paths.keys(), reverse=True)
-        max_depth = max(depths)        
-        for dp in depths:
-            for p in paths[dp]:
-                print p
-                if dp == max_depth:
-                    #  eq_p: equilibration path
-                    eq_p = os.path.join(p, eq_dir_name)
-                    os.rmdir(eq_p)
-                os.rmdir(p)
+    def testSedFiles(self):
+        prep.sed_files(self.core_vars, self.C['prep'], f_overwrite=True)
+
+    # def tearDown(self):
+    #     paths = const.DIR_DICT
+    #     for p in paths[1]:      # remove those of least depath
+    #         shutil.rmtree(p)
+
+
+        # for reference only, remove dir one by one
+        # depths = sorted(paths.keys(), reverse=True)
+        # max_depth = max(depths)        
+        # for dp in depths:
+        #     for p in paths[dp]:
+        #         print p
+        #         if dp == max_depth:
+        #             #  eq_p: equilibration path
+        #             eq_p = os.path.join(p, eq_dir_name)
+        #             os.rmdir(eq_p)
+        #         os.rmdir(p)
